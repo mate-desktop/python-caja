7c4ff1a2c72b9a4f2a84a2e10c7ea0450dbf5227
Patrick Monnerat <patrick@monnerat.net>
2019-01-19 01:29:22 +0100

    Include Python 3 ABI flags in shared library name.
    Python 3 appends the ABI flags to the version in the shared library file name:
    since this library is loaded as a gmodule, the correct file name must be
    provided.

--- src/caja-python.c
+++ src/caja-python.c
@@ -174,8 +174,8 @@ caja_python_init_python (void)
 	if (Py_IsInitialized())
 		return TRUE;
 
-  	debug("g_module_open " PY_LIB_LOC "/libpython" PYTHON_VERSION "." G_MODULE_SUFFIX ".1.0");
-	libpython = g_module_open(PY_LIB_LOC "/libpython" PYTHON_VERSION "." G_MODULE_SUFFIX ".1.0", 0);
+  	debug("g_module_open " PY_LIB_LOC "/libpython" PYTHON_VERSION PYTHON_ABIFLAGS "." G_MODULE_SUFFIX ".1.0");
+	libpython = g_module_open(PY_LIB_LOC "/libpython" PYTHON_VERSION PYTHON_ABIFLAGS "." G_MODULE_SUFFIX ".1.0", 0);
 	if (!libpython)
 		g_warning("g_module_open libpython failed: %s", g_module_error());
 
