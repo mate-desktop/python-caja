8b76a586a0316eff43ec291ae78bac47b0c31793
Erriez <Erriez@users.noreply.github.com>
2023-03-01 22:06:49 +0100

    Change shred menu into 4 submenu's
    Options:
    - 1x zero's
    - 1x overwrite
    - 3x overwrite, last zero's
    - 3x overwrite (default)

--- examples/shred.py
+++ examples/shred.py
@@ -3,10 +3,6 @@
 # This script adds a new menu `Secure delete` to Caja file browser when
 # right-mouse clicking on one or more selected files or directories.
 #
-# Note: Currently there is no Python-Caja example available to configure the
-# number of shred iterations via the Preferences, Configure Extension dialog.
-# https://github.com/mate-desktop/python-caja/issues/71
-#
 # Copy this script to a path defined in $XDG_DATA_DIRS, for example:
 #   ~/.local/share/caja-python/extensions
 
@@ -18,18 +14,12 @@ from gi.repository import Caja, GObject, Gio, Gtk
 
 
 class ShredMenuProvider(GObject.GObject, Caja.MenuProvider):
-    # Hard-coded settings
-    SHRED_ITERATIONS = 0
-    SHRED_EXECUTABLE = 'shred'
     SHRED_ICON = '/usr/share/pixmaps/caja/erase.png'
 
     def __init__(self):
-        self.shred_args = '-uz -n {}'.format(self.SHRED_ITERATIONS)
+        pass
 
-    def menu_activate_cb(self, menu, files):
-        if not files or not len(files):
-            return
-
+    def menu_activate_cb(self, menu, data):
         dialog = Gtk.MessageDialog(
             parent=None,
             flags=0,
@@ -41,20 +31,57 @@ class ShredMenuProvider(GObject.GObject, Caja.MenuProvider):
         response = dialog.run()
         dialog.destroy()
         if response == Gtk.ResponseType.YES:
-            shred_files = ''
-            for shred_file in files:
-                shred_files += '"{}" '.format(shred_file.get_location().get_path())
+            cmd = data['cmd']
+            for shred_file in data['files']:
+                cmd += ' "{}"'.format(shred_file.get_location().get_path())
 
             # Start Shred command
-            cmd = '{} {} {}'.format(self.SHRED_EXECUTABLE, self.shred_args, shred_files)
             subprocess.check_call(cmd, shell=True)
 
     def get_file_items(self, window, files):
-        top_menuitem = Caja.MenuItem(name='ShredMenuProvider::Shred', 
-                                     label='Secure delete', 
+        top_menuitem = Caja.MenuItem(name='ShredMenuProvider::Shred',
+                                     label='Secure delete',
                                      tip='Delete files with the Linux "shred" tool',
                                      icon=self.SHRED_ICON)
-        top_menuitem.connect('activate', self.menu_activate_cb, files)
+
+        shred_submenu = Caja.Menu()
+        top_menuitem.set_submenu(shred_submenu)
+
+        # Shred 1x zero's
+        shred_cmd = 'shred -uz -n 0'
+        shred_1x_zero_menuitem = Caja.MenuItem(name='ShredMenuProvider::Shred1xZero',
+                                               label='1x zero\'s',
+                                               tip=shred_cmd + ' FILE...',
+                                               icon=self.SHRED_ICON)
+        shred_1x_zero_menuitem.connect('activate', self.menu_activate_cb, {'cmd': shred_cmd, 'files': files})
+        shred_submenu.append_item(shred_1x_zero_menuitem)
+
+        # Shred 1x overwrite
+        shred_cmd = 'shred -u -n 0'
+        shred_1x_random_menuitem = Caja.MenuItem(name='ShredMenuProvider::Shred1x',
+                                                 label='1x overwrite',
+                                                 tip=shred_cmd + ' FILE...',
+                                                 icon=self.SHRED_ICON)
+        shred_1x_random_menuitem.connect('activate', self.menu_activate_cb, {'cmd': shred_cmd, 'files': files})
+        shred_submenu.append_item(shred_1x_random_menuitem)
+
+        # Shred 3x overwrite, last zero's
+        shred_cmd = 'shred -uz'
+        shred_3x_zero_menuitem = Caja.MenuItem(name='ShredMenuProvider::Shred3xLastZero',
+                                               label='3x overwrite, last zero\'s',
+                                               tip=shred_cmd + ' FILE...',
+                                               icon=self.SHRED_ICON)
+        shred_3x_zero_menuitem.connect('activate', self.menu_activate_cb, {'cmd': shred_cmd, 'files': files})
+        shred_submenu.append_item(shred_3x_zero_menuitem)
+
+        # Shred 3x overwrite (default)
+        shred_cmd = 'shred -u'
+        shred_3x_random_menuitem = Caja.MenuItem(name='ShredMenuProvider::Shred3x',
+                                                 label='3x overwrite (Default)',
+                                                 tip=shred_cmd + ' FILE...',
+                                                 icon=self.SHRED_ICON)
+        shred_3x_random_menuitem.connect('activate', self.menu_activate_cb, {'cmd': shred_cmd, 'files': files})
+        shred_submenu.append_item(shred_3x_random_menuitem)
 
         return top_menuitem,
 
